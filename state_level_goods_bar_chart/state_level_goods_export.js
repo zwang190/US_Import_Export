35fe15d4acaadb63f392d4d19c032882cb6c313e
zwang190 <zwang190@dons.usfca.edu>
2021-05-02 01:31:49 -0700

    modified bar chart.

--- state_level_goods_bar_chart/state_level_goods_export.js
+++ state_level_goods_bar_chart/state_level_goods_export.js
@@ -1,147 +1,211 @@
-d3.csv("state_level_goods_bar_chart/export_tx.csv", function (data) {
-    let grand_data = new Map()
-    let x_labels = ['2017', '2018', '2019', '2020'];
-    let y_labels = [];
-    let tmp = -1;
+let default_state = 'CA';
+d3.select('#dropdown_menu').select("#menu")
+    .on('change', function () {
+        default_state = this.value;
+        console.log(default_state);
+        d3.select("body").select("#dropdown_menu").select("#bar_chart").select("#bar_chart_2017").select("svg").remove();
+        d3.select("body").select("#dropdown_menu").select("#bar_chart").select("#bar_chart_2018").select("svg").remove();
+        d3.select("body").select("#dropdown_menu").select("#bar_chart").select("#bar_chart_2019").select("svg").remove();
+        d3.select("body").select("#dropdown_menu").select("#bar_chart").select("#bar_chart_2020").select("svg").remove();
+        create_bar_chart("2017", default_category, "#bar_chart_2017", "#pie1");
+        create_bar_chart("2018", default_category, "#bar_chart_2018", "#pie2");
+        create_bar_chart("2019", default_category, "#bar_chart_2019", "#pie3");
+        create_bar_chart("2020", default_category, "#bar_chart_2020", "#pie4");
+    });
 
-    for(let i = 0; i < data.length; i++) {
-        if(data[i]['rank'] == -1 || data[i]['rank'] == 0) {
-            continue;
-        }
+let default_category = 'Import';
+d3.select('#dropdown_menu').select("#category_menu")
+    .on('change', function () {
+        default_category = this.value;
+        console.log(default_category);
+        d3.select("body").select("#dropdown_menu").select("#bar_chart").select("#bar_chart_2017").select("svg").remove();
+        d3.select("body").select("#dropdown_menu").select("#bar_chart").select("#bar_chart_2018").select("svg").remove();
+        d3.select("body").select("#dropdown_menu").select("#bar_chart").select("#bar_chart_2019").select("svg").remove();
+        d3.select("body").select("#dropdown_menu").select("#bar_chart").select("#bar_chart_2020").select("svg").remove();
+        create_bar_chart("2017", default_category, "#bar_chart_2017", "#pie1");
+        create_bar_chart("2018", default_category, "#bar_chart_2018", "#pie2");
+        create_bar_chart("2019", default_category, "#bar_chart_2019", "#pie3");
+        create_bar_chart("2020", default_category, "#bar_chart_2020", "#pie4");
+    });
 
-        var name = data[i]['abbreviatn'];
-        var list = []
+function create_bar_chart(input_year, input_category, div, svg_id) {
+    d3.csv("pie_chart/ca_export_csv.csv", function (data) {
+        let grand_data = new Map()
+        let x_labels = ['2017', '2018', '2019', '2020'];
+        let y_labels = [];
+        let tmp = -1;
 
-        list.push({val2017: data[i]['val2017'], share17: data[i]['share17']});
-        list.push({val2018: data[i]['val2018'], share18: data[i]['share18']});
-        list.push({val2019: data[i]['val2019'], share19: data[i]['share19']});
-        list.push({val2020: data[i]['val2020'], share20: data[i]['share20']});
+        for (let i = 0; i < data.length; i++) {
+            var inner_map;
+            var category;
 
-        grand_data.set(name, list);
-        if (data[i]['rank'] > 5) {
-            break;
-        }
-    }
+            var state = data[i]['State'];
+            var rank = +data[i]['Rank'];
 
-    const margins = {top: 40, bottom: 40, left: 60, right: 70}
-    var width = 400;
-    var height = 800;
-    var bar_padding = 7;
-    var bar_width = (width / 4);
+            if (rank !== -1) {
+                if (!grand_data.has(state)) {
+                    inner_map = new Map();
+                    inner_map.set("Import", []);
+                    inner_map.set("Export", []);
+                    grand_data.set(state, inner_map);
+                }
 
-    var bar_svg = d3.select('body').select("#bar_chart")
-        .append('svg')
-        .attr('width', width + margins.left + margins.right)
-        .attr('height', height + margins.top + margins.bottom);
-
-    var bar_chart = bar_svg.append('g')
-        .attr('transform', 'translate(' + margins.right + ',' + margins.top + ')');
-
-    bar_svg.append("text")
-        .attr('x',0)
-        .attr("y", 30)
-        .text("Money flow (millions of dollars)")
-        .attr('font-family','sans-serif');
-
-    var inner_list = grand_data.get('PROCESSORS AND CONTROLLERS, ELECTRONIC INTEGRATED');
-
-    y_labels.push(inner_list[0].val2017);
-    y_labels.push(inner_list[1].val2018);
-    y_labels.push(inner_list[2].val2019);
-    y_labels.push(inner_list[3].val2020);
-
-    var bar_color_scale =
-        d3.scaleOrdinal(d3.schemeCategory10).domain(['2017', '2018', '2019', '2020']);
-
-    var x_scale = d3.scaleBand()
-        .rangeRound([0, width])
-        .domain(x_labels);
-
-    var y_scale = d3.scaleLinear()
-        .domain([0, 12000])
-        .range([height, 0]);
-
-    let x_axis = d3.axisBottom(x_scale);
-    let y_axis = d3.axisLeft(y_scale);
-
-    var legend = d3.legendColor()
-        .shapeHeight(25)
-        .shapeWidth(50)
-        .shape(40)
-        .orient('horizontal')
-        .scale(bar_color_scale);
-
-    bar_svg.append("g")
-        .attr("class", "legend")
-        .attr('font-size', '10px')
-        .attr('transform', 'translate(200,15)');
-
-    bar_chart.append('g')
-        .attr('transform', 'translate(0,' + height + ')')
-        .call(x_axis);
-
-    bar_chart.append('g')
-        .call(y_axis);
-
-    let hover_on = function(d) {
-        d3.selectAll(".bar")
-            .transition()
-            .duration(200)
-
-        d3.select(this)
-            .transition()
-            .duration(200)
-            .style("stroke", "black")
-            .style('stroke-width', '3px')
-    }
-
-    let hover_off = function(d, i) {
-        d3.selectAll(".bar")
-            .transition()
-            .duration(200)
-
-        d3.select(this)
-            .transition()
-            .duration(200)
-            .style("stroke", "white")
-    }
-
-    bar_chart.selectAll('.bar')
-        .data(y_labels)
-        .enter()
-        .append('rect')
-        .attr('y', function (d) {
-            return y_scale(d);
-        })
-        .attr('width', x_scale.bandwidth() - bar_padding)
-        .attr('height', function (d) {
-            return height - y_scale(d);
-        })
-        .attr('transform', function (d, i) {
-            var bar = [bar_width * i + 2, 0];
-            return 'translate(' + bar + ')';
-        })
-        .attr('fill', function (d, i) {
-            if (i === 0) {
-                return bar_color_scale('2017');
-            } else if(i === 1) {
-                return bar_color_scale('2018');
-            } else if (i === 2) {
-                return bar_color_scale('2019');
-            } else if(i === 3){
-                return bar_color_scale('2020');
+                inner_map = grand_data.get(state);
+                category = data[i]['Category'];
+                var inner_list = inner_map.get(category);
+                inner_list.push({
+                    name: data[i]['Description'],
+                    value2017: +data[i]['Value2017'],
+                    value2018: +data[i]['Value2018'],
+                    value2019: +data[i]['Value2019'],
+                    value2020: +data[i]['Value2020']
+                })
             }
-        })
-        .on("mouseover", hover_on)
-        .on("mouseleave", hover_off)
-        .append("title")
-        .text(function(d) { return d3.format("$,")(d); });
+        }
 
-    var ver_offset = 60;
-    var hor_offset = 60;
-    bar_svg.append("text")
-        .attr('x',width + hor_offset)
-        .attr("y", height + ver_offset)
-        .text("Year")
-        .attr('font-family','sans-serif');
-})
+        var new_data = [];
+        inner_map = grand_data.get(default_state);
+        inner_list = inner_map.get(input_category);
+
+        for (let i = 0; i < inner_list.length; i++) {
+            // new_data.push({name: inner_list[i].name, value: +inner_list[i].value2017})
+            if(input_year === '2017'){
+                new_data.push({name: inner_list[i].name, value: inner_list[i].value2017})
+            } else if(input_year === '2018'){
+                new_data.push({name: inner_list[i].name, value: inner_list[i].value2018})
+            } else if(input_year === '2019'){
+                new_data.push({name: inner_list[i].name, value: inner_list[i].value2019})
+            } else if(input_year === '2020'){
+                new_data.push({name: inner_list[i].name, value: inner_list[i].value2020})
+            }
+        }
+
+        new_data.sort(function (a, b) {
+            return b.value - a.value;
+        })
+
+        // const margins = {top: 40, bottom: 40, left: 60, right: 70}
+        // var width = 400;
+        // var height = 800;
+        // var bar_padding = 7;
+        // var bar_width = (width / 4);
+        //
+        // var bar_svg = d3.select('body').select("#dropdown_menu").select("#bar_chart").select(div)
+        //     .append('svg')
+        //     .attr('width', width + margins.left + margins.right)
+        //     .attr('height', height + margins.top + margins.bottom);
+        //
+        // var bar_chart = bar_svg.append('g')
+        //     .attr('transform', 'translate(' + margins.right + ',' + margins.top + ')');
+        //
+        // bar_svg.append("text")
+        //     .attr('x', 0)
+        //     .attr("y", 30)
+        //     .text("Money flow (millions of dollars)")
+        //     .attr('font-family', 'sans-serif');
+        //
+        // var inner_list = grand_data.get('PROCESSORS AND CONTROLLERS, ELECTRONIC INTEGRATED');
+        //
+        // y_labels.push(inner_list[0].val2017);
+        // y_labels.push(inner_list[1].val2018);
+        // y_labels.push(inner_list[2].val2019);
+        // y_labels.push(inner_list[3].val2020);
+        //
+        // var bar_color_scale =
+        //     d3.scaleOrdinal(d3.schemeCategory10).domain(['2017', '2018', '2019', '2020']);
+        //
+        // var x_scale = d3.scaleBand()
+        //     .rangeRound([0, width])
+        //     .domain(x_labels);
+        //
+        // var y_scale = d3.scaleLinear()
+        //     .domain([0, 12000])
+        //     .range([height, 0]);
+        //
+        // let x_axis = d3.axisBottom(x_scale);
+        // let y_axis = d3.axisLeft(y_scale);
+        //
+        // var legend = d3.legendColor()
+        //     .shapeHeight(25)
+        //     .shapeWidth(50)
+        //     .shape(40)
+        //     .orient('horizontal')
+        //     .scale(bar_color_scale);
+        //
+        // bar_svg.append("g")
+        //     .attr("class", "legend")
+        //     .attr('font-size', '10px')
+        //     .attr('transform', 'translate(200,15)');
+        //
+        // bar_chart.append('g')
+        //     .attr('transform', 'translate(0,' + height + ')')
+        //     .call(x_axis);
+        //
+        // bar_chart.append('g')
+        //     .call(y_axis);
+        //
+        // let hover_on = function (d) {
+        //     d3.selectAll(".bar")
+        //         .transition()
+        //         .duration(200)
+        //
+        //     d3.select(this)
+        //         .transition()
+        //         .duration(200)
+        //         .style("stroke", "black")
+        //         .style('stroke-width', '3px')
+        // }
+        //
+        // let hover_off = function (d, i) {
+        //     d3.selectAll(".bar")
+        //         .transition()
+        //         .duration(200)
+        //
+        //     d3.select(this)
+        //         .transition()
+        //         .duration(200)
+        //         .style("stroke", "white")
+        // }
+        //
+        // bar_chart.selectAll('.bar')
+        //     .data(y_labels)
+        //     .enter()
+        //     .append('rect')
+        //     .attr('y', function (d) {
+        //         return y_scale(d);
+        //     })
+        //     .attr('width', x_scale.bandwidth() - bar_padding)
+        //     .attr('height', function (d) {
+        //         return height - y_scale(d);
+        //     })
+        //     .attr('transform', function (d, i) {
+        //         var bar = [bar_width * i + 2, 0];
+        //         return 'translate(' + bar + ')';
+        //     })
+        //     .attr('fill', function (d, i) {
+        //         if (i === 0) {
+        //             return bar_color_scale('2017');
+        //         } else if (i === 1) {
+        //             return bar_color_scale('2018');
+        //         } else if (i === 2) {
+        //             return bar_color_scale('2019');
+        //         } else if (i === 3) {
+        //             return bar_color_scale('2020');
+        //         }
+        //     })
+        //     .on("mouseover", hover_on)
+        //     .on("mouseleave", hover_off)
+        //     .append("title")
+        //     .text(function (d) {
+        //         return d3.format("$,")(d);
+        //     });
+        //
+        // var ver_offset = 60;
+        // var hor_offset = 60;
+        // bar_svg.append("text")
+        //     .attr('x', width + hor_offset)
+        //     .attr("y", height + ver_offset)
+        //     .text("Year")
+        //     .attr('font-family', 'sans-serif');
+    })
+}
