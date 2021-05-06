f3d8e4c9b6dd4faa235a16e8e1918a901b7d6544
Zirong Wang <zirongwang@Zirongs-MacBook-Pro.local>
2021-05-06 10:54:23 -0700

    generalize line chart.

---
+++ national_import_export_five_categories_line_chart/generate_line_chart_import.js
@@ -0,0 +1,3 @@
+import {create_line_chart} from "./national_five_categories_linechart.js"
+
+create_line_chart("national_import_export_five_categories_line_chart/Import_five_end_use.csv", "#line_chart_import")
